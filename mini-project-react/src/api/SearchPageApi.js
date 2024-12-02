aba629b0ab20e5cc84d91a73cff9d13f14bfe8a2
GoMyeongho <sdx02013@ajou.ac.kr>
2024-12-02 17:47:02 +0900

    오늘의 마지막 커밋

--- mini-project-react/src/api/SearchPageApi.js
+++ mini-project-react/src/api/SearchPageApi.js
@@ -2,36 +2,48 @@ import axios from "axios";
 const KH_DOMAIN = "http://localhost:8111";
 
 const SearchApi = {
-	carListAll: async () => {
-		console.log("전체 차 리스트 불러오기");
-		return await axios.get(KH_DOMAIN + "/carList/all");
-	},
-	// 전체 검색을 돌리는 AxiosApi pages/search/SearchMain 주석 확인
+	
+	// 전체 검색을 돌리는 AxiosApi
+	//---------------------------- 국형씨 요청사항 -----------------------------
+	// pages/search/SearchMain 주석 확인
 	// 기본적으로 쿼리문을 전체 검색을 하는 쿼리문을 작성한 뒤
 	// 작성된 쿼리문에 문자열 연산자를 사용하여 WHERE 조건문을 넣는 스타일
 	// 만약 해당하는 값이 없으면 (null) 전체 검색이 되도록 추가 쿼리문을 붙이지 않도록 코드 작성
+	// * sort 는 정렬을 위한 옵션으로 추가 DB에 쓰진 않고 자바에서 Comparator 사용할 때 쓰기
 	// ex) carListSearch 를 name 과 price 와 engine 에 대해서 한다면
 	// 쿼리문은 SELECT CAR_NAME, PRICE, MANUFACTURER_NAME(VIEW 사용으로 조인 시켜놓기), ENGINE_TYPE FROM CARS(VIEW)
 	// + WHERE(만약 하나라도 null이 아니면 들어가게 만들기) + Name Like ?
 	// + AND(다른 값이 있다면 앞에 들어가게 만들기) + PRICE BETWEEN ? AND ?
 	// price 의 경우 최대 최소를 받아서 사용
-	// + AND ENGINE_TYPE = engine 엔진은 같다로 검색
-	carListSearch: async (name, company, price, engine) => {
+	// + AND ENGINE_TYPE in engine 엔진은 안에 있는지로 검색
+	// -------------------------------------------------------------------------
+	carListSearch: async (name, company, price, engine, sort) => {
 		console.log("조건에따라 검색하기\n" +
-			"검색 내용 : " + name + " - " + company + " - " + price + " - " + engine);
+			"검색 내용 : " + name + " - " + company + " - " + price + " - " + engine + " - " + sort);
 		const params = {
 			name: name,
 			company: company,
 			price: price,
 			engine: engine,
+			sort: sort
 		};
 		return await axios.get(KH_DOMAIN + "/carList/search",{ params });
 	},
+	// ------------------------- 국형씨 요청사항 ----------------------------
 	// 가능하면 이미지 링크 + 회사이름의 객체의 배열로 받아올 수 있도록
 	// 안된다면 이름의 배열로 받아오기
+	//---------------------------------------------------------------------
 	companiesList: async () => {
 		console.log("회사 목록 불러오기")
 		return await axios.get(KH_DOMAIN + "/carList/companies");
 	},
+	enginesList: async () => {
+		console.log("엔진 목록 불러오기")
+		return await axios.get(KH_DOMAIN + "/carList/engines");
+	},
+	maxPrice: async () => {
+		console.log("최대 가격 불러오기")
+		return await axios.get(KH_DOMAIN + "/carList/maxPrice");
+	}
 }
 export default SearchApi
--- mini-project-react/src/pages/search/SearchMain.js
+++ mini-project-react/src/pages/search/SearchMain.js
@@ -1,28 +1,55 @@
 import {useEffect, useState} from "react";
 import SearchApi from "../../api/SearchPageApi"
+import styled from "styled-components";
 
+const BoardContainer = styled.div`
+		width: 100%;
+		display: flex;
+`
+const SearchContainer = styled.div`
+		width: 30%;
+		display: flex;
+		flex-direction: column;
+`
 
+// {}에 감싸여 있는게 국형씨 요청사항
 const SearchMain = () => {
-	// 이름을 통해 검색을 할 때 사용할 Name
-	// 값이 있으면 Car_Name 에 Like ? 를 사용하여 값 추출
-	const [name, setName] = useState("");
-	// 회사 이름을 통해 검색하는 company
-	// 버튼을 통해 조작할거라  in 사용
-	const [company, setCompany] = useState("");
-	// 가격 범위를 통해 검색을 할 때 사용할 price
-	// isPrice 가 true 면 price 에 BETWEEN ? AND ? 를 사용하여 값 추출
-	const [price, setPrice] = useState({
-		isPrice: false,
-		minPrice: 0,
-		maxPrice: 999999999999,
-	});
-	// 엔진타입을 선택하기 위한 engine
-	// 마찬가지로 버튼으로 고르는 것이므로 in 사용
-	const [engine, setEngine] = useState("");
+	
 	// 회사 목록을 불러와서 저장할 리스트
 	const [companies, setCompanies] = useState([]);
+	// 엔진 목록을 불러와서 저장할 리스트
+	const [engines, setEngines] = useState([]);
+	// 최대 금액을 불러와서 저장할 변수
+	const [maxPrice, setMaxPrice] = useState(999999999);
+	
+	// 이름을 통해 검색을 할 때 사용할 Name
+	// {값이 있으면 Car_Name 에 Like ? 를 사용하여 값 추출}
+	const [name, setName] = useState("");
+	// 회사 이름을 통해 검색하는 company
+	// {버튼을 통해 조작할거라  in 사용}
+	const [company, setCompany] = useState("");
+	// 가격 범위를 통해 검색을 할 때 사용할 price
+	// {isPrice 가 true 면 price 에 BETWEEN ? AND ? 를 사용하여 값 추출}
+	const [price, setPrice] = useState({
+		isPrice: false,
+		min: 0,
+		max: maxPrice,
+	});
+	// 엔진타입을 선택하기 위한 engine
+	// {마찬가지로 버튼으로 고르는 것이므로 in 사용}
+	const [engine, setEngine] = useState("");
+	
+	const [sort, setSort] = useState({
+		sortBy: "name",
+		sortType: "desc",
+	})
+	
+	const onClickSearch = () => {}
+	
+	const value = { name, setName, company, setCompany, engine, setEngines, price, setPrice };
+	
 	useEffect(() => {
-		const company = async () => {
+		const getCompany = async () => {
 			try {
 				const rsp = await SearchApi.companiesList();
 				console.log(rsp.data);
@@ -31,18 +58,40 @@ const SearchMain = () => {
 				alert("회사 정보에 서버가 응답하지 않습니다.");
 			}
 		}
-		company();
+		const getEngine = async () => {
+			try {
+				const rsp = await SearchApi.enginesList();
+				console.log(rsp.data);
+				setEngines(rsp.data);
+			} catch (error) {
+				alert("엔진 정보에 서버가 응답하지 않습니다.")
+			}
+		}
+		const getMaxPrice = async () => {
+			try {
+				const rsp = await SearchApi.maxPrice();
+				console.log(rsp.data);
+				setMaxPrice(rsp.data);
+			} catch (error) {
+				alert("최대 가격에 서버가 응답하지 않습니다.")
+			}
+		}
+		getCompany();
+		getEngine();
+		getMaxPrice();
 	},[])
 	
 	
-	
-	
-	
-	
-	
 	return(
-		<>
-		
-		</>
+		<BoardContainer>
+			<SearchContainer>
+				<SearchOptions companies={companies} engines={engines} maxPrice={maxPrice}/>
+			</SearchContainer>
+			<ListConatiner>
+				<SearchOptionCloser/>
+				<SearchArrange/>
+				<SearchItems/>
+			</ListConatiner>
+		</BoardContainer>
 	)
 }
