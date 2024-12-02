6fb6c1e80b8ea9f3e21872c59a1104677756182e
GoMyeongho <sdx02013@ajou.ac.kr>
2024-12-02 16:44:42 +0900

    2024-12-02

--- mini-project-react/src/api/SearchPageApi.js
+++ mini-project-react/src/api/SearchPageApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 const KH_DOMAIN = "http://localhost:8111";
 
-const AxiosApi = {
+const SearchApi = {
 	carListAll: async () => {
 		console.log("전체 차 리스트 불러오기");
-		return await AxiosApi.get(KH_DOMAIN + "/carList/all");
+		return await axios.get(KH_DOMAIN + "/carList/all");
 	},
 	// 전체 검색을 돌리는 AxiosApi pages/search/SearchMain 주석 확인
 	// 기본적으로 쿼리문을 전체 검색을 하는 쿼리문을 작성한 뒤
@@ -16,10 +16,22 @@ const AxiosApi = {
 	// + AND(다른 값이 있다면 앞에 들어가게 만들기) + PRICE BETWEEN ? AND ?
 	// price 의 경우 최대 최소를 받아서 사용
 	// + AND ENGINE_TYPE = engine 엔진은 같다로 검색
-	carListSearch: async (name, company, price, engine, color, torque, power) => {
+	carListSearch: async (name, company, price, engine) => {
 		console.log("조건에따라 검색하기\n" +
-			"검색 내용 : " + name + " - " + company + " - " + price + " - " + engine + " - "+ color + " - " + torque + " - " + power);
-		return await AxiosApi.get(KH_DOMAIN + "/carList/search/", name, company, price, engine, color, torque, power);
+			"검색 내용 : " + name + " - " + company + " - " + price + " - " + engine);
+		const params = {
+			name: name,
+			company: company,
+			price: price,
+			engine: engine,
+		};
+		return await axios.get(KH_DOMAIN + "/carList/search",{ params });
 	},
-	
+	// 가능하면 이미지 링크 + 회사이름의 객체의 배열로 받아올 수 있도록
+	// 안된다면 이름의 배열로 받아오기
+	companiesList: async () => {
+		console.log("회사 목록 불러오기")
+		return await axios.get(KH_DOMAIN + "/carList/companies");
+	},
 }
+export default SearchApi
--- mini-project-react/src/pages/search/SearchMain.js
+++ mini-project-react/src/pages/search/SearchMain.js
@@ -1,14 +1,38 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import SearchApi from "../../api/SearchPageApi"
 
 
 const SearchMain = () => {
-	// 이름을 통해 검색을 할 때 사용할 searchName
-	// 값이 있으면 CarName 에 Like를 사용하여 값 추출
-	// 값이 없으면 쿼리문에 더하지 않게 사용
-	const [searchName, setSearchName] = useState("");
-	
-	const [price, setPrice] = useState("");
-	
+	// 이름을 통해 검색을 할 때 사용할 Name
+	// 값이 있으면 Car_Name 에 Like ? 를 사용하여 값 추출
+	const [name, setName] = useState("");
+	// 회사 이름을 통해 검색하는 company
+	// 버튼을 통해 조작할거라  in 사용
+	const [company, setCompany] = useState("");
+	// 가격 범위를 통해 검색을 할 때 사용할 price
+	// isPrice 가 true 면 price 에 BETWEEN ? AND ? 를 사용하여 값 추출
+	const [price, setPrice] = useState({
+		isPrice: false,
+		minPrice: 0,
+		maxPrice: 999999999999,
+	});
+	// 엔진타입을 선택하기 위한 engine
+	// 마찬가지로 버튼으로 고르는 것이므로 in 사용
+	const [engine, setEngine] = useState("");
+	// 회사 목록을 불러와서 저장할 리스트
+	const [companies, setCompanies] = useState([]);
+	useEffect(() => {
+		const company = async () => {
+			try {
+				const rsp = await SearchApi.companiesList();
+				console.log(rsp.data);
+				setCompanies(rsp.data);
+			} catch (error) {
+				alert("회사 정보에 서버가 응답하지 않습니다.");
+			}
+		}
+		company();
+	},[])
 	
 	
 	
