18641b2e2509c6b948737bc432665e3beb49988c
GoMyeongho <sdx02013@ajou.ac.kr>
2024-12-02 16:06:45 +0900

    2024-12-02

---
+++ mini-project-react/src/pages/search/SearchMain.js
@@ -0,0 +1,24 @@
+import {useState} from "react";
+
+
+const SearchMain = () => {
+	// 이름을 통해 검색을 할 때 사용할 searchName
+	// 값이 있으면 CarName 에 Like를 사용하여 값 추출
+	// 값이 없으면 쿼리문에 더하지 않게 사용
+	const [searchName, setSearchName] = useState("");
+	
+	const [price, setPrice] = useState("");
+	
+	
+	
+	
+	
+	
+	
+	
+	return(
+		<>
+		
+		</>
+	)
+}
