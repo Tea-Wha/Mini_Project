f1e1e4bcb1bb83bfae07a013f7561a172aff44cb
Cook <cook961004@gmail.com>
2024-12-12 14:23:51 +0900

    차량 상세정보 및 견적 페이지용 백엔드 구별 및 구현

--- miniproject/src/main/java/com/kh/miniproject/service/CustomiseService.java
+++ miniproject/src/main/java/com/kh/miniproject/service/CustomiseService.java
@@ -1,3 +1,4 @@
+/* 서비스 로직이 따로 필요 없어 각 repo 메서드 호출만 */
 package com.kh.miniproject.service;
 
 import com.kh.miniproject.repository.CustomiseRepository;
@@ -16,14 +17,17 @@ import java.util.List;
 public class CustomiseService {
     private final CustomiseRepository customiseRepository;
 
+    // 견적페이지 : repository 의 차량 불러오는 메서드 호출
     public List<CarVo> getCarInfo() {
         return customiseRepository.getCarInfo();
     }
 
+    // 견적페이지 : repository 의 차량색상 불러오는 메서드 호출
     public List<ColorVo> getColorInfo() {
         return customiseRepository.getColorInfo();
     }
 
+    // 견적페이지 : repository 의 차량옵션 불러오는 메서드 호출
     public List<FeatureVo> getFeatureInfo() {
         return customiseRepository.getFeatureInfo();
     }
