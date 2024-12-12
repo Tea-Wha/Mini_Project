6dfcec228ea0112e7d9652a51e8cb98fda7461bf
Tea-Wha <htw7880@gmail.com>
2024-12-12 17:57:07 +0900

    URL 서비스 수정

--- miniproject/src/main/java/com/kh/miniproject/controller/FirebaseGetUrlController.java
+++ miniproject/src/main/java/com/kh/miniproject/controller/FirebaseGetUrlController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class FirebaseGetUrlController {
@@ -24,5 +25,15 @@ public class FirebaseGetUrlController {
     @GetMapping("/api/images/shorturls") // Custom 상황 (축약 URL 받아오기) (Ex - 차 대표 / 상세 이미지, 컬러칩)
     public List<String> getShortImageUrls() throws IOException {
         return firebaseGetUrlService.getImageShortUrls();
+    }
+
+    @GetMapping("/api/images/fullurls/distribution") // Custom 상황 (전체 URL 받아오기) (다중 경로)
+    public List<List<String>> getAllImageUrlsDistribution() throws IOException {
+        return firebaseGetUrlService.getImageFullUrlsDistribution();
+    }
+
+    @GetMapping("/api/images/shorturls/distribution") // Custom 상황 (전체 URL 받아오기) (다중 경로)
+    public List<String> getShortImageUrlsDistribution() throws IOException {
+        return firebaseGetUrlService.getImageShortUrlsDistribution();
     }
 }
--- miniproject/src/main/java/com/kh/miniproject/service/FirebaseGetUrlService.java
+++ miniproject/src/main/java/com/kh/miniproject/service/FirebaseGetUrlService.java
@@ -9,17 +9,26 @@ import org.springframework.stereotype.Service;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class FirebaseGetUrlService {
 
     private final String firebaseCredentialsPath = "src/main/resources/firebase-service-account.json";
     private static final String BUCKET_NAME = "mini-project-d9c21.firebasestorage.app";
-    private static final String FOLDER_PATH = "IMAGE/CAR_LOGO/"; // Firebase의 경로 지정
-    private static final String FOLDER_SP_PATH = ""; // 상세 정보 관련 경로
+    private static final String FOLDER_PATH = "IMAGE/CAR_COLORCHIP/HYUNDAI/IONIQ6/"; // Firebase의 경로 지정
+    private static final String FOLDER_TOP_PATH = "IMAGE/CAR_SP_IMAGE/HYUNDAI/IONIQ6/"; // 상세 정보 관련 경로
+    private static final String[] FOLDER_PATHS = {
+            "IMAGE/CAR_SP_IMAGE/HYUNDAI/IONIQ6/A2B/",
+            "IMAGE/CAR_SP_IMAGE/HYUNDAI/IONIQ6/NY9/",
+            "IMAGE/CAR_SP_IMAGE/HYUNDAI/IONIQ6/R2P/",
+            "IMAGE/CAR_SP_IMAGE/HYUNDAI/IONIQ6/T2G/",
+            "IMAGE/CAR_SP_IMAGE/HYUNDAI/IONIQ6/W3T/",
+            "IMAGE/CAR_SP_IMAGE/HYUNDAI/IONIQ6/W6H/",
+            "IMAGE/CAR_SP_IMAGE/HYUNDAI/IONIQ6/XB9/"
+    }; // 다중 경로
 
+    // 전체 URL, 단일 경로 -> 컬러 칩 URL (상위 폴더에서)
     public List<String> getImageFullUrls() throws IOException {
         // Firebase Storage 초기화
         Storage storage = getStorage();
@@ -38,6 +47,7 @@ public class FirebaseGetUrlService {
         return imageUrls;
     }
 
+    // 축약 URL, 단일 경로 -> 컬러 칩 URL (상위 폴더에서)
     public List<String> getImageShortUrls() throws IOException {
         // Firebase Storage 초기화
         Storage storage = getStorage();
@@ -58,6 +68,111 @@ public class FirebaseGetUrlService {
         System.out.println("Image URL: " + imageUrls); // 확인용 URL
         return shortimageUrls;
     }
+
+    // 경로 나누기 + 전체 URL -> 차 이미지 URL (REP / SP) (다중 경로) (컬러 폴더대로 구분)
+    public List<List<String>> getImageFullUrlsDistribution() throws IOException {
+        // Firebase Storage 초기화
+        Storage storage = getStorage();
+
+        // 특정 경로의 파일 가져오기
+        Bucket bucket = storage.get(BUCKET_NAME);
+        List<List<String>> result = new ArrayList<>();
+
+        for (String folderPath : FOLDER_PATHS) {
+
+            List<String> imageUrls = new ArrayList<>();
+
+            // Blob 리스트 가져오기
+            for (Blob blob : bucket.list(Storage.BlobListOption.prefix(folderPath)).iterateAll()) {
+                // Blob URL 생성
+                String encodedPath = blob.getName().replace("/", "%2F");
+                String url = "https://firebasestorage.googleapis.com/v0/b/" + BUCKET_NAME + "/o/" + encodedPath + "?alt=media";
+                imageUrls.add(url); // 전체 URL 경로
+            }
+            result.add(imageUrls);
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    // 경로 나누기 + 축약 URL -> 차 이미지 URL (REP / SP) (다중 경로) (컬러 폴더대로 구분)
+//    public List<List<String>> getImageShortUrlsDistribution() throws IOException {
+//        // Firebase Storage 초기화
+//        Storage storage = getStorage();
+//
+//        // 특정 경로의 파일 가져오기
+//        Bucket bucket = storage.get(BUCKET_NAME);
+//        List<List<String>> result = new ArrayList<>();
+//        List<List<String>> shortResult = new ArrayList<>();
+//
+//        Set<String> subFolders = new HashSet<>();
+//
+//            // Blob 리스트 가져오기
+//            for (Blob blob : bucket.list(Storage.BlobListOption.prefix(FOLDER_TOP_PATH)).iterateAll()) {
+//                // Blob URL 생성
+//                String fullPath = blob.getName();
+//
+//                if (fullPath.startsWith(FOLDER_TOP_PATH)){
+//                    String relativePath = fullPath.substring(FOLDER_TOP_PATH.length());
+//                    String[] parts = relativePath.split("/");
+//                    System.out.println(relativePath);
+//
+//                    // 최상위 폴더 바로 아래의 폴더 이름 추가
+//                    if (parts.length > 1) {
+//                        subFolders.add(parts[0]);
+//                    }
+//                }
+//
+//            }
+//            // 각 하위 폴더를 순회하며 URL 리스트 생성
+//            for (String subFolder : subFolders){
+//                List<String> urls = new ArrayList<>();
+//                List<String> shorturls = new ArrayList<>();
+//                String folderPath = FOLDER_TOP_PATH + "/" + subFolder;
+////                System.out.println(folderPath);
+//
+//                for (Blob blob : bucket.list(Storage.BlobListOption.prefix(folderPath)).iterateAll()){
+//                    String encodedPath = blob.getName().replace("/", "%2F");
+//                    String shortPath = blob.getName().replace(FOLDER_TOP_PATH, "").replace("/", "%2F");
+//                    String url = "https://firebasestorage.googleapis.com/v0/b/" + BUCKET_NAME + "/o/" + encodedPath + "?alt=media";
+//                    urls.add(url);
+//                    shorturls.add(shortPath);
+//                }
+//                // 해당 하위 폴더 URL 리스트를 결과에 추가
+//                result.add(urls);
+//                shortResult.add(shorturls);
+//            }
+//        System.out.println(result);
+//        return shortResult;
+//    }
+public List<String> getImageShortUrlsDistribution() throws IOException {
+    // Firebase Storage 초기화
+    Storage storage = getStorage();
+
+    // 특정 경로의 파일 가져오기
+    Bucket bucket = storage.get(BUCKET_NAME);
+    List<List<String>> result = new ArrayList<>();
+    List<List<String>> shortResult = new ArrayList<>();
+
+    Set<String> subFolders = new LinkedHashSet<>();
+
+    // Blob 리스트 가져오기
+    for (Blob blob : bucket.list(Storage.BlobListOption.prefix(FOLDER_TOP_PATH)).iterateAll()) {
+        // Blob URL 생성
+        String fullPath = blob.getName();
+
+        if (fullPath.startsWith(FOLDER_TOP_PATH)){
+            String relativePath = fullPath.substring(FOLDER_TOP_PATH.length());
+            String[] parts = relativePath.split("/");
+
+            // 최상위 폴더 바로 아래의 폴더 이름 추가
+            if (parts.length > 1) {
+                subFolders.add(parts[0]);
+            }
+        }
+    }
+   return new ArrayList<>(subFolders);
+}
 
 
     private Storage getStorage() throws IOException {
